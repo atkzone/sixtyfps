1437f08c6739ffdbfe70f64bc0b0cba718bee9e1
Simon Hausmann <simon.hausmann@sixtyfps.io>
2022-01-20 20:31:57 +0100

    WIP

---
+++ examples/opengl_underlay/main.cpp
@@ -0,0 +1,9 @@
+// Copyright © SixtyFPS GmbH <info@sixtyfps.io>
+// SPDX-License-Identifier: (GPL-3.0-only OR LicenseRef-SixtyFPS-commercial)
+
+#include "gallery.h"
+
+int main()
+{
+    App::create()->run();
+}
---
+++ examples/opengl_underlay/main.rs
@@ -0,0 +1,23 @@
+// Copyright © SixtyFPS GmbH <info@sixtyfps.io>
+// SPDX-License-Identifier: (GPL-3.0-only OR LicenseRef-SixtyFPS-commercial)
+
+#![deny(unsafe_code)]
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+sixtyfps::include_modules!();
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub fn main() {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(all(debug_assertions, target_arch = "wasm32"))]
+    console_error_panic_hook::set_once();
+
+    let app = App::new();
+
+    app.window().set_rendering_notifier(|state| eprintln!("rendering state {:#?}", state));
+
+    app.run();
+}
